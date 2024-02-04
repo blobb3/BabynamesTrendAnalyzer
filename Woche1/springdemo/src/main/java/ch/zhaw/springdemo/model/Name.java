f7b7ddc56ecd044d9cc9fc455871db47767facee
blobb3 <janine.heinemeyer@gmail.com>
2024-02-04 16:04:13 +0100

    einpaartests

--- Woche1/springdemo/src/main/java/ch/zhaw/springdemo/model/Name.java
+++ Woche1/springdemo/src/main/java/ch/zhaw/springdemo/model/Name.java
@@ -1,45 +1,20 @@
 package ch.zhaw.springdemo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//Constructor für alle Attribute
+@AllArgsConstructor
+//Getter- und Setter-Methoden für alle Attribute
+@Getter
+@Setter
+// toString-Methode
+@ToString
+
 public class Name {
     private String name;
     private String geschlecht;
     private Integer anzahl;
-
-    // constructor
-    public Name(String name, String geschlecht, Integer anzahl) {
-        this.name = name;
-        this.geschlecht = geschlecht;
-        this.anzahl = anzahl;
-    }
-    
-
-    //getter and setter methods
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGeschlecht() {
-        return geschlecht;
-    }
-    public void setGeschlecht(String geschlecht) {
-        this.geschlecht = geschlecht;
-    }
-    
-    public Integer getAnzahl() {
-        return anzahl;
-    }
-    public void setAnzahl(Integer anzahl) {
-        this.anzahl = anzahl;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Name [name=" + name + ", geschlecht=" + geschlecht + ", anzahl=" + anzahl + "]";
-    }
-
-
 }
